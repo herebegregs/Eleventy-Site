f527cad5813e68d56087d28c05ffca4bd5c68381
carma-greg <129951032+carma-greg@users.noreply.github.com>
2024-10-18 18:31:03 +0100

    js working

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setIncludesDirectory("layouts");
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.setBrowserSyncConfig({
       notify: true,
       files: './_site/*.css'
