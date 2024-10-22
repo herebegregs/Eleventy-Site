621cdb891392511efe68d6318929365ae5024785
carma-greg <129951032+carma-greg@users.noreply.github.com>
2024-10-22 17:25:18 +0100

    modern update

--- _site/js/modern.js
+++ _site/js/modern.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
         let prevTab = tabs[i-1].getBoundingClientRect();
         tabs[i].style.top = prevTab.bottom+"px";
     }
-    let mainMenu = document.querySelector("#modern-menu .page-content");
+    let mainMenu = document.querySelector("[data-tab='links'] .page-content");
     let links = mainMenu.querySelectorAll("ul li[data-details]");
     let flyout = mainMenu.querySelector(".flyout");
     let flyoutDetails = mainMenu.querySelectorAll("[data-details");
@@ -45,4 +45,34 @@ document.addEventListener("DOMContentLoaded", function() {
             fadeOut = setTimeout(closeFlyoutDetails, 3000);
         })
     }
+
+
+    const elWrap = document.querySelector(".backdrop");
+    const elTilt = document.querySelector(".backdrop h1");
+    const settings = {
+        reverse: 0,        // Reverse tilt: 1, 0
+        max: 15,           // Max tilt: 35
+        perspective: 1000, // Parent perspective px: 1000
+        scale: 1,          // Tilt element scale factor: 1.0
+        axis: "",          // Limit axis. "y", "x"
+    };
+
+    elWrap.style.perspective = `${settings.perspective}px`;
+
+    const tilt = (evt) => {
+    const bcr = elWrap.getBoundingClientRect();
+    const x = Math.min(1, Math.max(0, (evt.clientX - bcr.left) / bcr.width));
+    const y = Math.min(1, Math.max(0, (evt.clientY - bcr.top) / bcr.height));
+    const reverse = settings.reverse ? -1 : 1;
+    const tiltX = reverse * (settings.max / 2 - x * settings.max);
+    const tiltY = reverse * (y * settings.max - settings.max / 2);
+    elTilt.style.transform = `
+        rotateX(${settings.axis === "x" ? 0 : tiltY}deg)
+        rotateY(${settings.axis === "y" ? 0 : tiltX}deg)
+        translateY(${settings.axis === "y" ? 0 : tiltY}px)
+        scale(${settings.scale})
+    `;
+    }
+
+    elWrap.addEventListener("pointermove", tilt);
 });
